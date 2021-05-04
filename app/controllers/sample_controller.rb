92d23aed240f9e849599cc6f3b8952c64a12c770
Glen Correia <glen_correia@outlook.com>
2021-05-04 12:13:20 +0530

    Remove sample_controller and related files

--- app/controllers/sample_controller.rb
+++
@@ -1,7 +0,0 @@
-class SampleController < ApplicationController
-  def delete_record
-    record = Book.find(params[:id]).destroy!
-
-    render json: { "message": "Record deleted successfully!" }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  delete 'sample/delete_record'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :books, only: [:index, :create, :destroy]
