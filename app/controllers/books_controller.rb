4a173af74e973d28986af5f97104818c5f34f93f
Glen Correia <glen_correia@outlook.com>
2021-05-04 10:24:20 +0530

    Add a create action to books_controller and check if records are saving properly to db

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,14 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(title: "Harry Potter 01", author: "JK Rowling")
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
 end
