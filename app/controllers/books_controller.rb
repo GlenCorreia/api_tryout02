b1128c6601176366081f562ee83d0393323305d7
Glen Correia <glen_correia@outlook.com>
2021-05-04 11:40:30 +0530

    Use a common functionality to call not_destroyed method whenever ActiveRecord::RecordNotDestroyed error is raised

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
   def index
     render json: Book.all # default 200 status code
   end
@@ -15,14 +17,16 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id]).destroy! # Find a book by the params hash
-    head :no_content
-  rescue ActiveRecord::RecordNotDestroyed
-    render json: {}, status: :unprocessable_entity
+    head :no_content    
   end
 
   private
 
     def book_params
       params.require(:book).permit(:title, :author)
+    end
+
+    def not_destroyed
+      render json: {}, status: :unprocessable_entity
     end
 end
