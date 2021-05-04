136ba4595bdbca784b5c79c398f8cae4ac4eba14
Glen Correia <glen_correia@outlook.com>
2021-05-04 10:40:37 +0530

    Use strict params to POST

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(title: params[:title], author: params[:author])
+    book = Book.new(book_params)
 
     if book.save
       render json: book, status: :created
@@ -12,4 +12,10 @@ class BooksController < ApplicationController
       render json: book.errors, status: :unprocessable_entity
     end
   end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :author)
+    end
 end
