d45767e00c4ac176ad1995ffee17ecd8ac250e1d
Glen Correia <glen_correia@outlook.com>
2021-05-05 18:08:27 +0530

    Move algorithm_type to a constant

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,11 +1,12 @@
 class AuthenticationTokenService
   HMAC_SECRET = 'sdfasdf'
+  ALGORITHM_TYPE = 'HS265'
 
   def self.call
     payload = {
       "test" => "blah"
     } 
 
-    JWT.encode payload, HMAC_SECRET, 'HS265'
+    JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
 end
