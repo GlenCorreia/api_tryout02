fa3c75f2ae36337b62ab48ece600566fafdd6ed1
Glen Correia <glen_correia@outlook.com>
2021-05-05 09:08:00 +0530

    Tidy up the test cases

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe 'Books API', type: :request do 
   describe 'GET /books' do
-    it 'returns all books' do 
+    before do # This before will get run before every test in this describe scope
       FactoryBot.create(:book, title: "1984", author: "George Orwell")
       FactoryBot.create(:book, title: "The Time Machine", author: "H.G. Wells")
+    end
+
+    it 'returns all books' do 
 
       get '/api/v1/books'
 
@@ -22,9 +25,10 @@ describe 'Books API', type: :request do
   end
 
   describe 'DELETE /books/:id' do 
-    book = FactoryBot.create(:book, title: "1984", author: "George Orwell")
+    let!(:book) { FactoryBot.create(:book, title: "1984", author: "George Orwell") }
+    
     it 'deletes a book' do 
-      delete "/api/v1/books/#{book.id}"
+    delete "/api/v1/books/#{book.id}"
       
       expect(response).to have_http_status(:no_content)
     end
