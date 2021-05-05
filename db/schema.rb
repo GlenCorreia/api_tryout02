8228b7ee71c48b26b6a96323896a1a80b98d80b9
Glen Correia <glen_correia@outlook.com>
2021-05-05 18:27:30 +0530

    Migrate up the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_041421) do
+ActiveRecord::Schema.define(version: 2021_05_05_125232) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -24,8 +27,14 @@ ActiveRecord::Schema.define(version: 2021_05_05_041421) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "author_id"
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
