30ab4a9622d3a5a3270567bdcb32b3ba148a978e
Glen Correia <glen_correia@outlook.com>
2021-05-05 18:46:34 +0530

    Finish up writing JWT based authentication service for username as of now

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,10 +4,12 @@ module Api
       rescue_from ActionController::ParameterMissing, with: :parameter_missing
       
       def create
-        p params.require(:username)
         p params.require(:password)
 
-        render json: { token: '123' }, status: :created
+        user = User.find_by(username: params.require(:username))
+        returned_token = AuthenticationTokenService.call(user.id)
+
+        render json: { token: returned_token }, status: :created
       end
 
       private
--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,11 +1,9 @@
 class AuthenticationTokenService
   HMAC_SECRET = 'sdfasdf'
-  ALGORITHM_TYPE = 'HS265'
+  ALGORITHM_TYPE = 'HS256'
 
-  def self.call
-    payload = {
-      "test" => "blah"
-    } 
+  def self.call(user_id)
+    payload = { user_id: user_id } 
 
     JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
